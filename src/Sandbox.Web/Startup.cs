86715005acf51915f8b05cf8351921c89c0e5df7
Ryan Nowak <nowakra@gmail.com>
2015-03-26 13:38:44 -0700

    Initial commit

---
+++ src/Sandbox.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Sandbox.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+    }
+}
