59ebc2718e67bfeb53fa50a5ea65be2031a586fd
Ryan Nowak <nowakra@gmail.com>
2015-07-07 16:43:59 -0700

    Add HelloWorld MVC App

---
+++ src/HelloWorldMvc/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+
+namespace HelloWorldMvc.Controllers
+{
+    public class HomeController 
+    {
+        [ActionContext]
+        public ActionContext ActionContext { get; set; }
+
+        public void Index()
+        {
+            ActionContext.HttpContext.Response.WriteAsync("Hello, World!");
+        }
+    }
+}
---
+++ src/HelloWorldMvc/Startup.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace HelloWorldMvc
+{
+    public class Startup
+    {
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMinimalMvc();
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
