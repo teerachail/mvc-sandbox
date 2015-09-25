75049d4a8814e048879713feafb5ae8f4aaf2aef
Ryan Nowak <nowakra@gmail.com>
2015-09-25 16:59:29 -0700

    Add HelloWorldMiddleware

---
+++ workloads/HelloWorldMiddleware/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace HelloWorldMiddleware
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                GC.KeepAlive(context.RequestServices);
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
