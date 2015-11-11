b87096445b144749b4004b7dd52983eec39c92c4
Ryan Nowak <nowakra@gmail.com>
2015-11-11 10:08:00 -0800

    Add large static page

---
+++ workloads/LargeStaticView/Startup.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LargeStaticView
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
