841840d2872193bcf3af49d8ce834c6a768f01e6
Doug Bunting <dougbu@microsoft.com>
2016-01-14 16:42:04 -0800

    Slow BigViews way way down

--- workloads/BigViews/loadtest_taghelpers.ps1
+++ workloads/BigViews/loadtest_taghelpers.ps1
@@ -1,4 +1,4 @@
 param([int] $iterations = 3000)
 
 $url = "http://127.0.0.1:5000/Home/IndexWithTagHelpers"
-& loadtest -k -n $iterations -c 16 --rps 20 $url
+& loadtest -k -n $iterations -c 16 --rps 10 $url
