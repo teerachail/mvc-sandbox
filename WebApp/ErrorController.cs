1d7bb704f3ee37f79beddbb3569c0b5c8d1c3041
Ryan Nowak <nowakra@gmail.com>
2016-05-06 10:17:52 -0700

    Add Diagnostics test project

---
+++ WebApp/ErrorController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var feature = HttpContext.Features.Get<IStatusCodePagesFeature>();
+            return View(HttpContext.Response.StatusCode);
+        }
+
+        public IActionResult Exception()
+        {
+            var feature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            return View(feature.Error);
+        }
+
+        public IActionResult ViewCompilationException()
+        {
+            return View();
+        }
+
+        public IActionResult ThrowHandled()
+        {
+            throw null;
+        }
+
+        public IActionResult ThrowingHandler()
+        {
+            throw new Exception("Exception thrown by handler");
+        }
+
+        public IActionResult ThrowingView()
+        {
+            return View();
+        }
+
+        public IActionResult Throw()
+        {
+            throw new Exception("Exception thrown by action.");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is NullReferenceException)
+            {
+                context.Result = new ObjectResult(new { error = context.Exception.Message })
+                {
+                    StatusCode = 500,
+                };
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,11 +30,61 @@ namespace WebApp
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMiddlewareAnalysis();
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, DiagnosticListener listener)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            listener.Subscribe(new ConsoleObserver());
+
+            //app.UseStatusCodePages();
+            //app.UseStatusCodePagesWithReExecute("~/status/{0}.html");
+            //app.UseStatusCodePagesWithReExecute("/Error/Index");
+
+            //app.UseExceptionHandler();
+            //app.UseExceptionHandler("/status/500.html");
+            //app.UseExceptionHandler("/error/Exception");
+            //app.UseExceptionHandler("/Error/ThrowingHandler");
+
+            //app.UseDeveloperExceptionPage();
+
+            //app.UseWelcomePage();
+
+            app.UseRuntimeInfoPage();
+
+            app.UseStaticFiles();
+
+            app.Map("/statuscode/set", b => b.Run(context =>
+            {
+                context.Response.StatusCode = int.Parse(context.Request.Query["code"]);
+                return Task.CompletedTask;
+            }));
+
+            app.UseMvcWithDefaultRoute();
+
+            app.Use(next => async context =>
+            {
+                await next(context);
+            });
+        }
+
+        private class ConsoleObserver : IObserver<KeyValuePair<string, object>>
+        {
+            public void OnCompleted()
+            {
+            }
+
+            public void OnError(Exception error)
+            {
+            }
+
+            public void OnNext(KeyValuePair<string, object> value)
+            {
+                Console.WriteLine($"{value.Key}: {value.Value}");
+            }
         }
     }
 }
